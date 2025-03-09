3a6e84bcdcca551c3addc7738047a9bb1db6fed5
Rafal EF <you@example.com>
2025-03-09 17:33:53 +0100

    update console log

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { issuer } from "@openauthjs/openauth";
-import { CloudflareStorage } from "@openauthjs/openauth/storage/cloudflare";
-import { PasswordProvider } from "@openauthjs/openauth/provider/password";
-import { PasswordUI } from "@openauthjs/openauth/ui/password";
-import { createSubjects } from "@openauthjs/openauth/subject";
-import { object, string } from "valibot";
+import { issuer } from '@openauthjs/openauth'
+import { CloudflareStorage } from '@openauthjs/openauth/storage/cloudflare'
+import { PasswordProvider } from '@openauthjs/openauth/provider/password'
+import { PasswordUI } from '@openauthjs/openauth/ui/password'
+import { createSubjects } from '@openauthjs/openauth/subject'
+import { object, string } from 'valibot'
 
 // This value should be shared between the OpenAuth server Worker and other
 // client Workers that you connect to it, so the types and schema validation are
@@ -12,7 +12,7 @@ const subjects = createSubjects({
   user: object({
     id: string(),
   }),
-});
+})
 
 export default {
   fetch(request: Request, env: Env, ctx: ExecutionContext) {
@@ -22,18 +22,18 @@ export default {
     // the application they came from. In our demo setup there is no other
     // application, so this Worker needs to do the initial redirect and handle
     // the callback redirect on completion.
-    const url = new URL(request.url);
-    if (url.pathname === "/") {
-      url.searchParams.set("redirect_uri", url.origin + "/callback");
-      url.searchParams.set("client_id", "your-client-id");
-      url.searchParams.set("response_type", "code");
-      url.pathname = "/authorize";
-      return Response.redirect(url.toString());
-    } else if (url.pathname === "/callback") {
+    const url = new URL(request.url)
+    if (url.pathname === '/') {
+      url.searchParams.set('redirect_uri', url.origin + '/callback')
+      url.searchParams.set('client_id', 'your-client-id')
+      url.searchParams.set('response_type', 'code')
+      url.pathname = '/authorize'
+      return Response.redirect(url.toString())
+    } else if (url.pathname === '/callback') {
       return Response.json({
-        message: "OAuth flow complete!",
+        message: 'OAuth flow complete!',
         params: Object.fromEntries(url.searchParams.entries()),
-      });
+      })
     }
 
     // The real OpenAuth server code starts here:
@@ -50,32 +50,32 @@ export default {
               // This is where you would email the verification code to the
               // user, e.g. using Resend:
               // https://resend.com/docs/send-with-cloudflare-workers
-              console.log(`Sending code ${code} to ${email}`);
+              console.log('codemail: ' + code)
             },
             copy: {
-              input_code: "Code (check Worker logs)",
+              input_code: 'Code (check Worker logs)',
             },
           }),
         ),
       },
       theme: {
-        title: "myAuth",
-        primary: "#0051c3",
-        favicon: "https://workers.cloudflare.com//favicon.ico",
+        title: 'myAuth',
+        primary: '#0051c3',
+        favicon: 'https://workers.cloudflare.com//favicon.ico',
         logo: {
-          dark: "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public",
+          dark: 'https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/db1e5c92-d3a6-4ea9-3e72-155844211f00/public',
           light:
-            "https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public",
+            'https://imagedelivery.net/wSMYJvS3Xw-n339CbDyDIA/fa5a3023-7da9-466b-98a7-4ce01ee6c700/public',
         },
       },
       success: async (ctx, value) => {
-        return ctx.subject("user", {
+        return ctx.subject('user', {
           id: await getOrCreateUser(env, value.email),
-        });
+        })
       },
-    }).fetch(request, env, ctx);
+    }).fetch(request, env, ctx)
   },
-} satisfies ExportedHandler<Env>;
+} satisfies ExportedHandler<Env>
 
 async function getOrCreateUser(env: Env, email: string): Promise<string> {
   const result = await env.AUTH_DB.prepare(
@@ -87,10 +87,10 @@ async function getOrCreateUser(env: Env, email: string): Promise<string> {
 		`,
   )
     .bind(email)
-    .first<{ id: string }>();
+    .first<{ id: string }>()
   if (!result) {
-    throw new Error(`Unable to process user: ${email}`);
+    throw new Error(`Unable to process user: ${email}`)
   }
-  console.log(`Found or created user ${result.id} with email ${email}`);
-  return result.id;
+  console.log(`Found or created user ${result.id} with email ${email}`)
+  return result.id
 }
